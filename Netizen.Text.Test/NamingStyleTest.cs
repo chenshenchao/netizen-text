8534ff7a5463b7281e0e52d261c9bc5f76571f4d
chenshenchao <chenshenchao@outlook.com>
2022-03-12 20:50:49 +0800

    unittest

--- Netizen.Text.Test/NamingStyleTest.cs
+++ Netizen.Text.Test/NamingStyleTest.cs
@@ -29,6 +29,37 @@ namespace Netizen.Text.Test
         }
 
         [TestMethod]
+        [DataRow("testName", "test-name")]
+        [DataRow("test-data", "test-data")]
+        [DataRow(" is done", "is-done")]
+        [DataRow(" letIt-go ", "let-it-go")]
+        [DataRow("-anyBad Name", "any-bad-name")]
+        [DataRow("HTTP_METHOD", "http-method")]
+        public void TestKebabCase(string source, string result)
+        {
+            string r = source.ToKebabCase();
+            Assert.AreEqual(result, r);
+        }
+
+        [TestMethod]
+        [DataRow("testName", "testName")]
+        [DataRow("test-data", "testData")]
+        [DataRow(" is done", "isDone")]
+        [DataRow(" letIt-go ", "letItGo")]
+        [DataRow("-anyBad Name", "anyBadName")]
+        [DataRow("HTTP_METHOD", "httpMethod","httpMETHOD")]
+        public void TestCamelCase(string source, string result, string? fresult = null)
+        {
+            string r = source.ToCamelCase();
+            Assert.AreEqual(result, r);
+            if (fresult != null)
+            {
+                string fr = source.ToCamelCase(false);
+                Assert.AreEqual(fresult, fr);
+            }
+        }
+
+        [TestMethod]
         [DataRow("testName", "TestName")]
         [DataRow("test-data", "TestData")]
         [DataRow(" is done", "IsDone")]
--- Readme.md
+++ Readme.md
@@ -1,5 +1,14 @@
 # [Netizen.Text](https://github.com/chenshenchao/netizen-text)
 
+## 安装
+
+```bash
+# dotnet
+dotnet add package Netizen.Text
+# Package Manager
+Install-Package Netizen.Text
+```
+
 ## 打包
 
 ```bash
